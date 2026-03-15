4a8526002a3c504688d5bb6b66f15eab12459f06
nikhilmahato21 <nikhilmahato3337@gmail.com>
2026-03-15 21:07:07 +0530

    feat:render link added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://sylla-2.onrender.com",
         changeOrigin: true,
       },
     },
